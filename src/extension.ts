f4082034284761884754d6471f79f6dc25782c34
Henning Dieterichs <henning.dieterichs@live.de>
2020-05-10 11:18:25 +0200

    Removes extension hot reload.

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,12 @@
 import * as vscode from "vscode";
 import { Disposable } from "@hediet/std/disposable";
-import {
-	enableHotReload,
-	hotRequireExportedFn,
-	registerUpdateReconciler,
-	getReloadCount,
-} from "@hediet/node-reload";
-
-if (process.env.HOT_RELOAD) {
-	enableHotReload({ entryModule: module, loggingEnabled: true });
-}
-
 import { DrawioEditorProvider } from "./DrawioEditorProvider";
 import { DrawioTextEditorProvider } from "./DrawioTextEditorProvider";
-
-registerUpdateReconciler(module);
 
 export class Extension {
 	public readonly dispose = Disposable.fn();
 
 	constructor() {
-		if (getReloadCount(module) > 0) {
-			const i = this.dispose.track(vscode.window.createStatusBarItem());
-			i.text = "reload" + getReloadCount(module);
-			i.show();
-		}
-
 		this.dispose.track(
 			vscode.window.registerCustomEditorProvider(
 				"hediet.vscode-drawio-text",
@@ -53,9 +34,7 @@ export class Extension {
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		hotRequireExportedFn(module, Extension, (Extension) => new Extension())
-	);
+	context.subscriptions.push(new Extension());
 }
 
 export function deactivate() {}
