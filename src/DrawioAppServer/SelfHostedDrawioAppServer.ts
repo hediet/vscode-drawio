216e57d12b95b527be5646643b2dcb26bb981cac
Vincent Rouillé <vincent@speedy37.fr>
2020-05-20 01:42:41 +0200

    use webpack and reduce package size

--- src/DrawioAppServer/SelfHostedDrawioAppServer.ts
+++ src/DrawioAppServer/SelfHostedDrawioAppServer.ts
@@ -1,14 +1,12 @@
 import { HostedDrawioAppServer } from "./HostedDrawioAppServer";
 import * as path from "path";
-import * as fs from "fs";
 import { Webview, Uri } from "vscode";
+import html from './vscode.html';
 
 export class SelfHostedDrawioAppServer extends HostedDrawioAppServer {
 
 	public async getHtml(webview: Webview): Promise<string> {
-		const index = path.join(__dirname, "../../vscode.html");
-		const html = await fs.promises.readFile(index, { encoding: "utf8" });
-		const vsuri = webview.asWebviewUri(Uri.file(path.join(__dirname, "../../drawio/src/main/webapp/index.html")));
+		const vsuri = webview.asWebviewUri(Uri.file(path.join(__dirname, "../drawio/src/main/webapp/index.html")));
 		const patchedHtml = html
 			.replace("${vsuri}", vsuri.toString())
 			.replace("${theme}", this.getTheme())
