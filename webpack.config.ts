091f1c0c593a8ab3dd584ca34b77b0472e518de6
Henning Dieterichs <henning.dieterichs@live.de>
2020-05-20 11:27:15 +0200

    Uses typescript for webpack configuration.

---
+++ webpack.config.ts
@@ -0,0 +1,44 @@
+import * as webpack from "webpack";
+import path = require("path");
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+
+const r = (file: string) => path.resolve(__dirname, file);
+
+module.exports = {
+	target: "node",
+	entry: r("./src/extension"),
+	output: {
+		path: r("./out"),
+		filename: "extension.js",
+		libraryTarget: "commonjs2",
+		devtoolModuleFilenameTemplate: "../[resource-path]",
+	},
+	devtool: "source-map",
+	externals: {
+		vscode: "commonjs vscode",
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/i,
+				loader: "raw-loader",
+			},
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "ts-loader",
+					},
+				],
+			},
+		],
+	},
+	node: {
+		__dirname: false,
+	},
+	plugins: [new CleanWebpackPlugin()],
+} as webpack.Configuration;
