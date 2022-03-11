2b35e3c0e23fe04fdf3c3c4bb463af00fc5b054f
Kevin Dai <kevindai02@outlook.com>
2022-03-11 09:45:53 -0500

    Fixed SymbolKind record to compute indicies
    - Ran prettier on code. npm run lint passes :)

--- src/features/CodeLinkFeature.ts
+++ src/features/CodeLinkFeature.ts
@@ -15,7 +15,7 @@ import {
 	QuickPickItem,
 	QuickPickOptions,
 	DocumentSymbol,
-	SymbolKind
+	SymbolKind,
 } from "vscode";
 import { wait } from "@hediet/std/timer";
 import { DrawioEditorService, DrawioEditor } from "../DrawioEditorService";
@@ -35,34 +35,34 @@ const linkWsSymbolWithSelectedNodeCommandName =
 const linkFileWithSelectedNodeCommandName =
 	"hediet.vscode-drawio.linkFileWithSelectedNode";
 
-const symbolNameMap: Record<SymbolKind, string> = [
-	'symbol-file',
-	'symbol-module',
-	'symbol-namespace',
-	'symbol-package',
-	'symbol-class',
-	'symbol-method',
-	'symbol-property',
-	'symbol-field',
-	'symbol-constructor',
-	'symbol-enum',
-	'symbol-interface',
-	'symbol-function',
-	'symbol-variable',
-	'symbol-constant',
-	'symbol-string',
-	'symbol-number',
-	'symbol-boolean',
-	'symbol-array',
-	'symbol-object',
-	'symbol-key',
-	'symbol-null',
-	'symbol-enum-member',
-	'symbol-struct',
-	'symbol-event',
-	'symbol-operator',
-	'symbol-type-parameter'
-];
+const symbolNameMap: Record<SymbolKind, string> = {
+	[SymbolKind.File]: "symbol-file",
+	[SymbolKind.Module]: "symbol-module",
+	[SymbolKind.Namespace]: "symbol-namespace",
+	[SymbolKind.Package]: "symbol-package",
+	[SymbolKind.Class]: "symbol-class",
+	[SymbolKind.Method]: "symbol-method",
+	[SymbolKind.Property]: "symbol-property",
+	[SymbolKind.Field]: "symbol-field",
+	[SymbolKind.Constructor]: "symbol-constructor",
+	[SymbolKind.Enum]: "symbol-enum",
+	[SymbolKind.Interface]: "symbol-interface",
+	[SymbolKind.Function]: "symbol-function",
+	[SymbolKind.Variable]: "symbol-variable",
+	[SymbolKind.Constant]: "symbol-constant",
+	[SymbolKind.String]: "symbol-string",
+	[SymbolKind.Number]: "symbol-number",
+	[SymbolKind.Boolean]: "symbol-boolean",
+	[SymbolKind.Array]: "symbol-array",
+	[SymbolKind.Object]: "symbol-object",
+	[SymbolKind.Key]: "symbol-key",
+	[SymbolKind.Null]: "symbol-null",
+	[SymbolKind.EnumMember]: "symbol-enum-member",
+	[SymbolKind.Struct]: "symbol-struct",
+	[SymbolKind.Event]: "symbol-event",
+	[SymbolKind.Operator]: "symbol-operator",
+	[SymbolKind.TypeParameter]: "symbol-type-parameter",
+};
 
 export class LinkCodeWithSelectedNodeService {
 	public readonly dispose = Disposable.fn();
@@ -90,9 +90,9 @@ export class LinkCodeWithSelectedNodeService {
 						if (activeEditor) {
 							this.statusBar.text = `$(link) ${
 								activeEditor.config.codeLinkActivated
-								? "$(circle-filled)"
-								: "$(circle-outline)"
-								} Code Link`;
+									? "$(circle-filled)"
+									: "$(circle-outline)"
+							} Code Link`;
 							this.statusBar.show();
 						} else {
 							this.statusBar.hide();
@@ -125,7 +125,7 @@ export class LinkCodeWithSelectedNodeService {
 			registerFailableCommand(
 				linkWsSymbolWithSelectedNodeCommandName,
 				this.linkWsSymbolWithSelectedNode
-			)
+			),
 		]);
 	}
 
@@ -160,7 +160,10 @@ export class LinkCodeWithSelectedNodeService {
 			return;
 		}
 
-		const pos = new DeserializedCodePosition(editor.document.uri, editor.selection);
+		const pos = new DeserializedCodePosition(
+			editor.document.uri,
+			editor.selection
+		);
 		lastActiveDrawioEditor.drawioClient.linkSelectedNodeWithData(
 			pos.serialize(lastActiveDrawioEditor.uri)
 		);
@@ -188,7 +191,9 @@ export class LinkCodeWithSelectedNodeService {
 	}
 
 	@action.bound
-	private async linkSymbolWithSelectedNode(storeTopLevelSymbol: boolean = false) {
+	private async linkSymbolWithSelectedNode(
+		storeTopLevelSymbol: boolean = false
+	) {
 		const lastActiveDrawioEditor =
 			this.editorManager.lastActiveDrawioEditor;
 		if (!lastActiveDrawioEditor) {
@@ -201,9 +206,12 @@ export class LinkCodeWithSelectedNodeService {
 			return;
 		}
 		const uri = editor.document.uri;
-		const hasSelection = !editor.selection.start.isEqual(editor.selection.end);
+		const hasSelection = !editor.selection.start.isEqual(
+			editor.selection.end
+		);
 		const result = (await commands.executeCommand(
-			"vscode.executeDocumentSymbolProvider", uri
+			"vscode.executeDocumentSymbolProvider",
+			uri
 		)) as DocumentSymbol[];
 		let items: QuickPickItem[] = [];
 		function recurse(symb: DocumentSymbol[], path: string) {
@@ -211,10 +219,15 @@ export class LinkCodeWithSelectedNodeService {
 				// If there is a selection and we do not intersect it, omit the symbol
 				let intersectSelection = true;
 				if (hasSelection && editor) {
-					intersectSelection =
-						editor.selections.reduce((prev: boolean, cur) => {
-							return prev && cur.intersection(x.selectionRange) !== undefined;
-						}, intersectSelection);
+					intersectSelection = editor.selections.reduce(
+						(prev: boolean, cur) => {
+							return (
+								prev &&
+								cur.intersection(x.selectionRange) !== undefined
+							);
+						},
+						intersectSelection
+					);
 				}
 				// Add the symbol and descend into children
 				let curpath = path == "" ? x.name : `${path}.${x.name}`;
@@ -222,28 +235,39 @@ export class LinkCodeWithSelectedNodeService {
 					items.push(<QuickPickItem>{
 						label: `$(${symbolNameMap[x.kind]}) ${x.name}`,
 						description: x.detail,
-						detail: curpath
+						detail: curpath,
 					});
 				}
 				recurse(x.children, curpath);
 			}
 		}
 		recurse(result, "");
-		window.showQuickPick(items, <QuickPickOptions>{
-			matchOnDescription: true,
-			matchOnDetail: true,
-			placeHolder: `Choose symbol from ${path.basename(uri.fsPath)}`
-		}).then(async v => {
-			if (v == undefined) return;
-			const pos: CodePosition = new CodePosition(storeTopLevelSymbol ? undefined : uri, v.detail);
-			lastActiveDrawioEditor.drawioClient.linkSelectedNodeWithData(
-				pos.serialize(lastActiveDrawioEditor.uri)
-			);
-			// Validate upon exist, as some languages do not export workspace symbols
-			if (storeTopLevelSymbol && v.detail && !(await resolveTopSymbol(v.detail))) {
-				window.showWarningMessage(`Cannot resolve symbol ${v.detail}. This likely means workspace symbols are not supported by your language. Try "Link Symbol With Selected Node" instead.`);
-			}
-		});
+		window
+			.showQuickPick(items, <QuickPickOptions>{
+				matchOnDescription: true,
+				matchOnDetail: true,
+				placeHolder: `Choose symbol from ${path.basename(uri.fsPath)}`,
+			})
+			.then(async (v) => {
+				if (v == undefined) return;
+				const pos: CodePosition = new CodePosition(
+					storeTopLevelSymbol ? undefined : uri,
+					v.detail
+				);
+				lastActiveDrawioEditor.drawioClient.linkSelectedNodeWithData(
+					pos.serialize(lastActiveDrawioEditor.uri)
+				);
+				// Validate upon exist, as some languages do not export workspace symbols
+				if (
+					storeTopLevelSymbol &&
+					v.detail &&
+					!(await resolveTopSymbol(v.detail))
+				) {
+					window.showWarningMessage(
+						`Cannot resolve symbol ${v.detail}. This likely means workspace symbols are not supported by your language. Try "Link Symbol With Selected Node" instead.`
+					);
+				}
+			});
 	}
 
 	private handleDrawioEditor(editor: DrawioEditor): void {
@@ -269,7 +293,10 @@ export class LinkCodeWithSelectedNodeService {
 
 			if (linkedData) {
 				try {
-					const pos = await CodePosition.deserialize(linkedData, editor.uri);
+					const pos = await CodePosition.deserialize(
+						linkedData,
+						editor.uri
+					);
 					await this.revealSelection(pos);
 				} catch (e) {
 					window.showErrorMessage((e as Error).message);
@@ -293,7 +320,9 @@ export class LinkCodeWithSelectedNodeService {
 
 	private lastDecorationType: TextEditorDecorationType | undefined;
 
-	private async revealSelection(pos: DeserializedCodePosition): Promise<void> {
+	private async revealSelection(
+		pos: DeserializedCodePosition
+	): Promise<void> {
 		if (pos.range) {
 			const d = await workspace.openTextDocument(pos.uri);
 			const e = await window.showTextDocument(d, {
@@ -332,7 +361,10 @@ class CodePosition {
 	public readonly range: Range | undefined;
 	private readonly symbol: string | undefined;
 
-	public static async deserialize(value: unknown, relativeTo: Uri): Promise<DeserializedCodePosition> {
+	public static async deserialize(
+		value: unknown,
+		relativeTo: Uri
+	): Promise<DeserializedCodePosition> {
 		const data = value as Data;
 		function getPosition(pos: PositionData): Position {
 			return new Position(pos.line, pos.col);
@@ -347,19 +379,26 @@ class CodePosition {
 				path: Uri.file(path.join(relativeTo.path, data.path)).path,
 			});
 			if ("start" in data) {
-				let range = new Range(getPosition(data.start), getPosition(data.end));
+				let range = new Range(
+					getPosition(data.start),
+					getPosition(data.end)
+				);
 				return new DeserializedCodePosition(uri, range);
 			} else if ("symbol" in data) {
 				let range = await resolveSymbol(uri, data.symbol);
 				if (range == undefined)
-					throw new Error(`Cannot find symbol by path: ${data.symbol}. Maybe you need to load the symbols by opening at least one of its code files?`);
+					throw new Error(
+						`Cannot find symbol by path: ${data.symbol}. Maybe you need to load the symbols by opening at least one of its code files?`
+					);
 				return new DeserializedCodePosition(uri, range);
 			}
 			return new DeserializedCodePosition(uri, undefined);
 		} else if ("symbol" in data) {
 			let pos = await resolveTopSymbol(data.symbol);
 			if (pos) return pos;
-			throw new Error(`Cannot find symbol by path: ${data.symbol}. Maybe you need to load the symbols by opening at least one of its code files?`);
+			throw new Error(
+				`Cannot find symbol by path: ${data.symbol}. Maybe you need to load the symbols by opening at least one of its code files?`
+			);
 		}
 
 		// Exceptions will be very rare in this case
@@ -367,7 +406,10 @@ class CodePosition {
 		throw new Error(`Malformed symbol information. Check console log.`);
 	}
 
-	constructor(public readonly uri: Uri | undefined, private obj?: Range | string) {
+	constructor(
+		public readonly uri: Uri | undefined,
+		private obj?: Range | string
+	) {
 		if (obj instanceof Range) {
 			this.range = obj as Range;
 		} else if (typeof obj == "string") {
@@ -391,7 +433,7 @@ class CodePosition {
 			};
 		} else if (this.symbol) {
 			rangeObj = {
-				symbol: this.symbol
+				symbol: this.symbol,
 			};
 		}
 
@@ -400,11 +442,11 @@ class CodePosition {
 				path: path
 					.relative(relativeTo.fsPath, this.uri.fsPath)
 					.replace(/\\/g, "/"),
-				...rangeObj
+				...rangeObj,
 			};
 		} else {
 			return <Data>{
-				...rangeObj
+				...rangeObj,
 			};
 		}
 	}
@@ -412,7 +454,7 @@ class CodePosition {
 
 // CodePosition after deserializing (from Data object)
 class DeserializedCodePosition {
-	constructor(public readonly uri: Uri, public readonly range?: Range) { }
+	constructor(public readonly uri: Uri, public readonly range?: Range) {}
 	public serialize(relativeTo: Uri): Data {
 		return new CodePosition(this.uri, this.range).serialize(relativeTo);
 	}
@@ -421,15 +463,15 @@ class DeserializedCodePosition {
 type Data = {
 	path: string | undefined;
 } & (
-		| {}
-		| {
+	| {}
+	| {
 			start: PositionData;
 			end: PositionData;
-		}
-		| {
+	  }
+	| {
 			symbol: string;
-		}
-	);
+	  }
+);
 
 interface PositionData {
 	line: number;
@@ -442,25 +484,28 @@ function getSorterBy<T>(selector: (item: T) => number) {
 	};
 }
 
-async function resolveSymbol(uri: Uri, path: string): Promise<Range | undefined> {
+async function resolveSymbol(
+	uri: Uri,
+	path: string
+): Promise<Range | undefined> {
 	const result = (await commands.executeCommand(
-		"vscode.executeDocumentSymbolProvider", uri
+		"vscode.executeDocumentSymbolProvider",
+		uri
 	)) as DocumentSymbol[];
-	let treePath = path.split('.');
+	let treePath = path.split(".");
 	let cur: DocumentSymbol[] | undefined = result;
 	for (let i = 0; i < treePath.length; i++) {
-		if (cur == undefined)
-			break;
-		cur = cur.filter(x => x.name == treePath[i]);
-		if (i < treePath.length - 1)
-			cur = cur[0]?.children;
+		if (cur == undefined) break;
+		cur = cur.filter((x) => x.name == treePath[i]);
+		if (i < treePath.length - 1) cur = cur[0]?.children;
 	}
-	if (cur == undefined || cur.length == 0)
-		return undefined;
+	if (cur == undefined || cur.length == 0) return undefined;
 	return cur[0].selectionRange;
 }
 
-async function resolveTopSymbol(path: string): Promise<DeserializedCodePosition | undefined> {
+async function resolveTopSymbol(
+	path: string
+): Promise<DeserializedCodePosition | undefined> {
 	let res = path.split(".");
 	if (res.length == 0) return undefined;
 	// res.length > 0
@@ -468,37 +513,33 @@ async function resolveTopSymbol(path: string): Promise<DeserializedCodePosition 
 	if (!symb) return undefined;
 	if (res.length == 2) {
 		const range = await resolveSymbol(symb.uri, path);
-		if (range)
-			symb = new DeserializedCodePosition(symb.uri, range);
+		if (range) symb = new DeserializedCodePosition(symb.uri, range);
 	}
 	return symb;
 }
 
-async function resolveWorkspaceSymbol(symbolName: string): Promise<DeserializedCodePosition | undefined> {
+async function resolveWorkspaceSymbol(
+	symbolName: string
+): Promise<DeserializedCodePosition | undefined> {
 	const result = (await commands.executeCommand(
 		"vscode.executeWorkspaceSymbolProvider",
 		symbolName
 	)) as SymbolInformation[];
-	for (let x of result)
-		console.log(x.name);
+	for (let x of result) console.log(x.name);
 	const filtered = result
 		.filter((r) => r.name === symbolName)
 		.sort(
 			getSorterBy((matchedSymbol) => {
 				let score = 0;
 
-				const uriAsString =
-					matchedSymbol.location.uri.toString();
+				const uriAsString = matchedSymbol.location.uri.toString();
 
 				const idx = window.visibleTextEditors.findIndex(
-					(e) =>
-						e.document.uri.toString() ===
-						uriAsString
+					(e) => e.document.uri.toString() === uriAsString
 				);
 				if (idx !== -1) {
 					score +=
-						(window.visibleTextEditors.length -
-							idx) /
+						(window.visibleTextEditors.length - idx) /
 						window.visibleTextEditors.length;
 				}
 
