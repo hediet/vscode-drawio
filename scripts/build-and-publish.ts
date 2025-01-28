8bbbd94159b775f557628efa42433dc2986f3702
Henning Dieterichs <hdieterichs@microsoft.com>
2025-01-28 17:35:38 +0100

    update

--- scripts/build-and-publish.ts
+++ scripts/build-and-publish.ts
@@ -1,9 +1,10 @@
-import { getChangelog } from "./shared";
 import { readFile, writeFile } from "fs/promises";
-import { resolve } from "path";
+import { join, resolve } from "path";
 import { SemanticVersion } from "@hediet/semver";
 import { context } from "@actions/github";
 import { exec } from "@actions/exec";
+import { readFileSync } from "fs";
+import { Changelog } from "./changelog";
 
 const packageJsonPath = resolve(__dirname, "../package.json");
 
@@ -11,41 +12,51 @@ export async function run(): Promise<void> {
 	const changeLog = getChangelog();
 	const version = changeLog.latestVersion;
 
-	const packageJson = await readJsonFile<{ version: string }>(packageJsonPath);
+	const packageJson = await readJsonFile<{ version: string; preRelease?: boolean }>(packageJsonPath);
 	const stableVersion = SemanticVersion.parse(packageJson.version);
 
 	if (version.kind !== 'released' || version.version.toString() !== stableVersion.toString()) {
 		throw new Error("Version in package.json does not match latest version in changelog.");
 	}
 
+	if (stableVersion.patch !== 0) {
+		throw new Error("Patch version must be 0.");
+	}
+
 	const gh = new GitHubClient();
 
+	const isPreRelease = !!packageJson.preRelease;
+
 	const stableTag = `v${stableVersion}`;
-	if (!await gh.tagExists(context.repo, stableTag)) {
-		try {
-			await publish('stable', stableVersion);
-		} catch (e) { }
-		//await gh.createTag(context.repo, stableTag, context.sha);
+	const stableGhTagExists = await gh.tagExists(context.repo, stableTag);
+	if (!isPreRelease && !stableGhTagExists) {
+		await buildAndPublish('stable', stableVersion);
+		await gh.createTag(context.repo, stableTag, context.sha);
 	} else {
-		console.log(`GitHub tag for stable version ${stableTag} exists, skipping publish.`);
+		if (stableGhTagExists) {
+			console.log(`GitHub tag for stable version ${stableTag} exists, skipping publish.`);
+		}
+		if (isPreRelease) {
+			console.log("Pre-release version detected, skipping stable");
+		}
 	}
 
 	const runNumber = process.env.GITHUB_RUN_NUMBER;
-	const preReleaseNumber = `${new Date().toISOString().replace(/[^0-9]/g, '')}0${runNumber}`;
+	const preReleaseNumber = `${formatDate(new Date())}0${Number(runNumber)}`;
 	const previewVersion = stableVersion.with({ patch: Number(preReleaseNumber) });
 
 	const previewTag = `v${previewVersion}`;
-	if (!await gh.tagExists(context.repo, previewTag)) {
-		try {
-			await publish('preRelease', previewVersion);
-		} catch (e) { }
-		//await gh.createTag(context.repo, previewTag, context.sha);
+
+	const previewGhTagExists = await gh.tagExists(context.repo, previewTag);
+	if (!previewGhTagExists) {
+		await buildAndPublish('preRelease', previewVersion);
+		await gh.createTag(context.repo, previewTag, context.sha);
 	} else {
 		console.log(`GitHub tag for preview version ${previewTag} exists, skipping publish.`);
 	}
 }
 
-async function publish(releaseType: 'stable' | 'preRelease', version: SemanticVersion) {
+async function buildAndPublish(releaseType: 'stable' | 'preRelease', version: SemanticVersion) {
 	console.log(`Publishing ${releaseType} version ${version}...`);
 
 	const packageJson = await readJsonFile<any>(packageJsonPath);
@@ -62,9 +73,22 @@ async function publish(releaseType: 'stable' | 'preRelease', version: SemanticVe
 		"vsce",
 		"publish",
 		"--pat",
-		process.env.VSCE_TOKEN!,
+		process.env.VSCE_PAT!,
 		...(releaseType === 'preRelease' ? ['--pre-release'] : [])]
 	);
+}
+
+
+function padN(num: number, n: number): string {
+	return num.toString().padStart(n, '0');
+}
+
+function formatDate(date: Date): string {
+	const year = date.getUTCFullYear();
+	const month = date.getUTCMonth() + 1;
+	const day = date.getUTCDate();
+
+	return `${year}${padN(month, 2)}${padN(day, 2)}`;
 }
 
 interface IRepo {
@@ -118,4 +142,12 @@ async function readJsonFile<T>(path: string): Promise<T> {
 async function writeJsonFile(path: string, data: unknown): Promise<void> {
 	const content = JSON.stringify(data, null, '\t');
 	await writeFile(path, content, 'utf-8');
+}
+
+function readTextFileSync(fileName: string): string {
+	return readFileSync(fileName, { encoding: "utf-8" });
+}
+
+function getChangelog(): Changelog {
+	return new Changelog(readTextFileSync(join(__dirname, "../CHANGELOG.md")));
 }
